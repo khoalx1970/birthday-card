3a7b92314c4e767b7b88ebaf36488dd32abba77e
khoalx1970 <vovodangkhoa15@gmail.com>
2024-07-31 21:40:02 +0700

    Add files via upload
    birthday card

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/joyqt.004';
+const eventName = 'Happy 20th Birthday Joy';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
